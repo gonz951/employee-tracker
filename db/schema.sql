3661d66fe7a4d576500f428c52577aaa5462de94
Fel <felgonzalez951@gmail.com>
2024-05-20 17:50:58 -0600

    added DO method, minor tweaks to the tables work

--- db/schema.sql
+++ db/schema.sql
@@ -3,28 +3,42 @@ DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 \c company_db;
 
-CREATE TABLE department (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
-);
+DO $$
+DECLARE 
 
-CREATE TABLE role (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL NOT NULL,
-    department INTEGER NOT NULL
-    FOREIGN KEY (department)
-    REFERENCES department(id)
-);
+BEGIN
 
-CREATE TABLE employee (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-    FOREIGN KEY (role_id) 
-    REFERENCES role(id),
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-);
+
+    CREATE TABLE department (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(30) UNIQUE NOT NULL
+    );
+
+    CREATE TABLE roles (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(30) UNIQUE NOT NULL,
+        salary DECIMAL NOT NULL,
+        department INTEGER NOT NULL,
+        FOREIGN KEY (department)
+        REFERENCES department(id)
+    );
+
+    CREATE TABLE employee (
+        id SERIAL PRIMARY KEY,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        role_id INTEGER NOT NULL,
+        manager_id INTEGER,
+        FOREIGN KEY (role_id) 
+        REFERENCES roles(id),
+        FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
+    );
+
+RAISE NOTICE 'Transaction Complete';
+
+EXCEPTION 
+    WHEN OTHERS THEN 
+        RAISE NOTICE 'An error occurred: %', SQLERRM;
+        ROLLBACK;
+END $$; 
