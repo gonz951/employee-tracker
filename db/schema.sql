b34c253fcc25346590ba6e7f8343815199969d08
Fel <felgonzalez951@gmail.com>
2024-05-20 15:04:46 -0600

    moved around the foreign key commands

--- db/schema.sql
+++ db/schema.sql
@@ -21,9 +21,10 @@ CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
     FOREIGN KEY (role_id) 
     REFERENCES role(id),
-    manager_id INTEGER
-    
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
