b8462e1db32425c46a6e8ece236e651482c62d90
Fel <felgonzalez951@gmail.com>
2024-05-20 18:13:11 -0600

    added host and port to pool const

--- db/connection.js
+++ db/connection.js
@@ -4,6 +4,8 @@ const pool = new Pool(
     {
         user: 'postgres',
         password: 'bootgres',
+        host: 'localhost',
+        port: 5432,
         database: 'company_db'
     },
     console.log(`Connected to the company_db`)
