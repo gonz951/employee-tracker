d7bceb95c7e8626f9cb8a5a94e63d37fa7093003
Fel <felgonzalez951@gmail.com>
2024-05-21 17:45:39 -0600

    finished findallroles(), findalldepartments(), and still tweaking with findallemployees()

--- db/index.js
+++ db/index.js
@@ -15,21 +15,31 @@ class DB {
     
     findAllDepartments() {
         return this.query(
-            'SELECT department.id, department.name FROM department;'
-        )
+            // * Working
+            'SELECT * FROM department;'
+        );
     }
 
     findAllRoles() {
         return this.query(
-            // ! THIS ONE DOESNT WORK
-            'SELECT roles.id, roles.title, roles.department, roles.salary FROM roles LEFT JOIN department ON roles.department = department.name;'
-        )
+            // * Working
+            'SELECT roles.id, roles.title, department.name AS department, roles.salary FROM department JOIN roles ON roles.department = department.id;'
+        );
     }
 
     findAllEmployees() {
         return this.query(
-            // ! THIS ONE ISNT FINISHED
-            'SELECT employee.id, employee.first_name, roles.title, department.name AS department'
-        )
+            // ! THIS ONE ISNT FINISHED OR WORKING
+            // need to figure out how to show manager
+            'SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department, roles.salary FROM roles JOIN employee ON employee.role_id = roles.id JOIN department ON roles.department = department.id;'
+        );
+    }
+
+    addDepartment() {
+        return this.query(
+            `INSERT INTO department (name) VALUES (${newDepartment});`
+        );
     }
 }
+
+module.exports = DB
